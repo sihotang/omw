77ca00bc337fb45892b35bb4bdb45f3452a9daeb
Sihotang <soparsihotang@gmail.com>
2017-05-14 07:55:42 +0000

    New gulp scripts for minify scripts and make sourcemaps for script files

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,25 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('storage/app/styles'));
 });
 
+/* Task: Compile Scripts
+--------------------------------------------------------------------------------- */
+gulp.task('scripts', function() {
+    gulp.src([
+            'resources/scripts/**/*.js'
+        ])
+        .pipe(plugins.newer('storage/app/scripts'))
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.babel())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('storage/app/scripts'))
+        .pipe(plugins.concat('main.min.js'))
+        .pipe(plugins.uglify({ preserveComments: 'some' }))
+        .pipe(plugins.size({ title: 'scripts' }))
+        .pipe(plugins.sourcemaps.write('.'))
+        .pipe(gulp.dest('app/scripts'))
+        .pipe(gulp.dest('storage/app/scripts'))
+});
+
 /* Task: Clean & Clean Cache
 --------------------------------------------------------------------------------- */
 gulp.task('clean', () => del(['storage/app/*', '!storage/app/.gitignore'], { dot: true }));
@@ -45,7 +64,7 @@ gulp.task('clean:cache', () => del(['storage/app/*', '!storage/app/.gitignore'],
 --------------------------------------------------------------------------------- */
 gulp.task('default', ['clean'], cb =>
     sequence(
-        'styles',
+        'styles', ['scripts'],
         cb
     )
 );
